3a7ae249fe81320ecc5ee4a1c183362abd5985ae
Piyush Dubey <dubeypiyush@microsoft.com>
2022-07-25 17:19:56 +0530

    first commit

---
+++ RewardsApp.TeamsApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RewardsApp.TeamsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            ViewBag.AboutApp = "Reward your Teammated with NFTs.";
+            return View();
+        }
+
+        [Route("hello")]
+        public ActionResult Hello()
+        {
+            return View("Index");
+        }
+
+        [Route("first")]
+        public ActionResult First()
+        {
+            return View();
+        }
+
+        [Route("second")]
+        public ActionResult Second()
+        {
+            return View();
+        }
+
+        [Route("configure")]
+        public ActionResult Configure()
+        {
+            return View();
+        }
+
+    }
+}
