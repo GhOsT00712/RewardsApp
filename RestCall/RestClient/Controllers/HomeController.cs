7d29f590f493b83adcb30cfe8eb08ff3b951091b
Aditya Singh Sisodiya <asisodiya@microsoft.com>
2022-09-20 22:01:04 +0530

    feat: add rest client app

---
+++ RestCall/RestClient/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RestClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+
+
+
+        //Hosted web API REST Service base url
+        string Baseurl = "http://192.168.95.1:5555/";
+        public async Task<ActionResult> Index()
+        {
+            List<userData> EmpInfo = new List<userData>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                HttpResponseMessage Res = await client.GetAsync("api/Employee/GetAllEmployees");
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    //Deserializing the response recieved from web api and storing into the Employee list
+                    EmpInfo = JsonConvert.DeserializeObject<List<userData>>(EmpResponse);
+                }
+                //returning the employee list to view
+                return View(EmpInfo);
+            }
+        }
+    }
+}
---
+++ RestCall/RestClient/Models/userData.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Security.Policy;
+using System;
+
+namespace RestClient.Models
+{
+    public class userData
+    {
+        public List<OwnedNft> ownedNfts { get; set; }
+        public int totalCount { get; set; }
+    }
+    public class Attribute
+    {
+        public string value { get; set; }
+        public string trait_type { get; set; }
+    }
+    public class Contract
+    {
+        public string address { get; set; }
+    }
+    public class Medium
+    {
+        public string raw { get; set; }
+        public string gateway { get; set; }
+    }
+    public class OwnedNft
+    {
+        public Contract contract { get; set; }
+        public string tokenId { get; set; }
+        public string tokenType { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public DateTime timeLastUpdated { get; set; }
+        public RawMetadata rawMetadata { get; set; }
+        public TokenUri tokenUri { get; set; }
+        public List<Medium> media { get; set; }
+        public int balance { get; set; }
+    }
+    public class RawMetadata
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string image { get; set; }
+        public List<Attribute> attributes { get; set; }
+    }
+    public class TokenUri
+    {
+        public string raw { get; set; }
+        public string gateway { get; set; }
+    }
+}
