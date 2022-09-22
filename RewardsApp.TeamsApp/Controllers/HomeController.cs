86ce18ea92281e4c9aa0965026521fca1958adb1
Piyush Dubey <dubeypiyush@microsoft.com>
2022-09-22 13:38:09 +0530

    UI changes view nft

--- RewardsApp.TeamsApp/Controllers/HomeController.cs
+++ RewardsApp.TeamsApp/Controllers/HomeController.cs
@@ -59,7 +59,7 @@ namespace RewardsApp.TeamsApp.Controllers
         {
             try
             {
-                emailId = "vishnu.gupta@microsoft.com";
+                emailId = "dubeypiyush@microsoft.com";
                 string connectionString = "DefaultEndpointsProtocol=https;AccountName=hacktable-db;AccountKey=avNvpJakzTfzBgcBbps413YHX2ykpbRPf7S7ZcHBL99Yt4GqgzbIAouoxotyvRf7DocgDYRboBTk7AEek653qQ==;TableEndpoint=https://hacktable-db.table.cosmos.azure.com:443/;";
                 TableServiceClient tableServiceClient = new TableServiceClient(connectionString);
                 TableClient tableClient = tableServiceClient.GetTableClient(
@@ -113,9 +113,9 @@ namespace RewardsApp.TeamsApp.Controllers
                 foreach (OwnedNft ownedNft in ownedNfts)
                 {
                     Dictionary<String, String> metaData = new Dictionary<String, String>();
-                    metaData.Add("name", ownedNft.rawMetadata?.name);
-                    metaData.Add("description", ownedNft.rawMetadata?.description);
                     metaData.Add("image", ownedNft.rawMetadata?.image);
+                    foreach(var attibute in ownedNft.rawMetadata.attributes)
+                    metaData.Add(attibute.trait_type, attibute.value);
                     result.Add(metaData);
                 }
                 return result;
